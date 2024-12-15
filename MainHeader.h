d91cd32ba9e8d607fb419c101197380187a934dd
Llihc <falih.qwerty13@gmail.com>
2024-12-15 20:25:55 +0700

    Creating main header

---
+++ MainHeader.h
@@ -0,0 +1,11 @@
+#ifndef MAINHEADER_H_INCLUDED
+#define MAINHEADER_H_INCLUDED
+#include "conio.h"
+#include "windows.h"
+#include "iostream"
+#include "stdlib.h"
+#include "cstdio"
+using namespace std;
+
+
+#endif // MAINHEADER_H_INCLUDED
