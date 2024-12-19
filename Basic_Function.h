fbfdbed3ab4bb6613166fadf1f51062c380e0f03
Llihc <falih.qwerty13@gmail.com>
2024-12-20 03:37:16 +0700

    Insertion and deletion #done

---
+++ Basic_Function.h
@@ -0,0 +1,10 @@
+#ifndef BASIC_FUNCTION_H_INCLUDED
+#define BASIC_FUNCTION_H_INCLUDED
+
+adrTxt createTxtElem(infotype x);
+void createLine();
+void add(infotype x);
+void backspace();
+void delete_elem();
+
+#endif // BASIC_FUNCTION_H_INCLUDED
