e618e2fca30b21babc9c383fb601a11070777f3a
Llihc <falih.qwerty13@gmail.com>
2024-12-20 01:06:07 +0700

    Set Print function

--- Display.cpp
+++ Display.cpp
@@ -1,47 +1,12 @@
 #include "MainHeader.h"
 
-void DefaultDisplay(){
+void DefaultDisplay(bool &typing){
     int input;
     string mode_input;
     bool first = true;
 
     IntroText("Command");
-    while(true){
-        input = getch();
-        if (input == 58){
-            cout << char(input);
-            cin >> mode_input;
-            if (mode_input == "q!"){
-                system("CLS");
-                return;
-            } else {
-                system("CLS");
-                IntroText("Command");
-                continue;
-            }
-        } else {
-            if (input == 27 && first){
-                system("CLS");
-                IntroText("Command");
-                continue;
-            } else if (first){
-                system("CLS");
-                IntroText("Insert");
-                first = false;
-            }else {
-                system("CLS");
-            }
-            while(true){
-                input = getch();
-                if (input == 27){
-                    break;
-                } else{
-                    system("CLS");
-                    cout <<  cout << input << char(input) << endl; //Dingati list text nanti
-                }
-            }
-        }
-    }
+    typing = true;
 }
 
 void IntroText(string mode){
@@ -54,8 +19,7 @@ void IntroText(string mode){
     printf("------------------------------------------------------------------------------------------------------------------------\n");
 }
 
-void ShowConsoleCursor(bool showFlag)
-{
+void ShowConsoleCursor(bool showFlag){
     HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
 
     CONSOLE_CURSOR_INFO     cursorInfo;
@@ -65,7 +29,24 @@ void ShowConsoleCursor(bool showFlag)
     SetConsoleCursorInfo(out, &cursorInfo);
 }
 
+void printLine(adrLine P){
+    adrTxt temp = P->TxtFirst;
+    while (temp){
+        cout << temp->info;
+        temp = temp->next;
+    }
+    cout << endl;
+}
 
+void Print(){
+    int numline = 0;
+    adrLine Line_tmp = Line.First;
+    while(Line_tmp != NULL){
+        printf("Line %d :", ++numline);
+        printLine(Line_tmp);
+        Line_tmp = Line_tmp->next;
+    }
+}
 
 
 
