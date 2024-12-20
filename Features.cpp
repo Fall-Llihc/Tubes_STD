31d6ea31e2c3aae5a1eeb686c9ab4e5a8975dca4
Llihc <falih.qwerty13@gmail.com>
2024-12-20 08:51:39 +0700

    Update Overall #4

---
+++ Features.cpp
@@ -0,0 +1,11 @@
+#include "MainHeader.h"
+
+adrHistory createHistoryElem(string tipe){
+    adrHistory P = new history;
+    P->tipe = tipe;
+    P->line = cursor.line_info;
+    P->txtelem = cursor.txt_info;
+    P->next = NULL;
+    P->prev = NULL;
+    return P;
+}
---
+++ History_and_Features.h
@@ -0,0 +1,19 @@
+#ifndef HISTORY_AND_FEATURES_H_INCLUDED
+#define HISTORY_AND_FEATURES_H_INCLUDED
+
+typedef struct history* adrHistory;
+
+struct history{
+    string tipe;
+    adrLine line;
+    adrTxt txtelem;
+    adrHistory next;
+    adrHistory prev;
+};
+
+struct ListHistory(){
+    adrHistory First;
+    adrHistory Last;
+};
+
+#endif // HISTORY_AND_FEATURES_H_INCLUDED
